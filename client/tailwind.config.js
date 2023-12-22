e29e8c6a650fc59b3ae23d4c2475f812f320ea58
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-12-22 02:51:48 -1200

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    // Add other file paths as needed
+  ],
+  // ... other Tailwind CSS configurations
+};
