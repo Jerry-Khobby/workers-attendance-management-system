ae115bd2207e35dd1e4bcff455f57cc6468885e5
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-12-22 03:03:10 -1200

    change my mind I am using a framework now

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,10 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.html',
-    './src/**/*.js',
-    // Add other file paths as needed
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  // ... other Tailwind CSS configurations
-};
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
