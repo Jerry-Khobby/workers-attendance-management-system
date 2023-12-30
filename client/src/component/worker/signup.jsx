b6ae8529e284ff69d1270a96c3fab0a5e45c2e55
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-12-29 12:18:03 -1200

    cant find the bug here

--- client/src/component/worker/signup.jsx
+++ client/src/component/worker/signup.jsx
@@ -2,90 +2,108 @@ import React, { useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import axios from 'axios';
 
 const SignUpForm = () => {
-  const [passwordMatch, setPasswordMatch] = useState(true);
-  const handleSubmit = async (event) => {
+//Handle the form submission , 
+// handle the changing of the value in the field 
+const [formData, setFormData] = useState({
+  firstName: '',
+  lastName: '',
+  email: '',
+  telePhone: '',
+  password: '',
+  confirmPassword: '',
+  profilePicture: null,
+  dateOfBirth: '',
+  age: '',
+  placeOfStay: '',
+  previousExperience: '',
+  acceptTerms: false,
+});
 
-    event.preventDefault();
-    if (!passwordMatch) {
-      console.log('Passwords do not match. Please check and try again.');
-      return;
-    }
+const handleChange = (event) => {
+  const { name, value, type, checked, files } = event.target;
 
-    try {
-      // Make a POST request to your Django backend
-      const response = await fetch('http://127.0.0.1:8000/api/users/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          ...basicInfo,
-          ...additionalInfo,
-        }),
-      });
+  // Special handling for file input
+  if (type === 'file') {
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: files[0],
+    }));
+  } else if (type === 'checkbox') {
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: checked,
+    }));
+  } else {
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+};
 
-      if (response.ok) {
-        console.log('Form submitted successfully!');
-        // Add any additional logic after a successful submission
-      } else {
-        console.error('Error submitting the form:', response.statusText);
-        // Handle the error as needed
-      }
-    } catch (error) {
-      console.error('Error:', error.message);
-      // Handle the error as needed
-    }
-  };
-
-  const [basicInfo, setBasicInfo] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    telePhone: '',
-    password: '',
-    confirmPassword: '',
-    profilePicture: null,
-  });
-
-  const [additionalInfo, setAdditionalInfo] = useState({
-    dateOfBirth: '',
-    placeOfStay: '',
-    previousExperience: '',
-    acceptTerms: false,
-    age:''
-  });
-
-  const handleBasicInfoChange = (event) => {
-    const { name, value } = event.target;
-      // Validate telephone number to be exactly 10 digits
-  if (name === 'telephone' && !/^\d{10}$/.test(value)) {
-    // Display an error message or handle it as needed
-    console.log('Invalid telephone number. Please enter exactly 10 digits.');
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  // Check if passwords match
+  if (formData.password !== formData.confirmPassword) {
+    // Display error message or handle password mismatch as needed
+    console.error('Passwords do not match');
     return;
   }
-    setBasicInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
+
+  // Create a new object with only the necessary fields
+  const userData = {
+    first_name: formData.firstName,
+    last_name: formData.lastName,
+    email: formData.email,
+    telephone: formData.telePhone,
+    password: formData.confirmPassword,  // Send confirm password if they match
+    image: formData.profilePicture,
+    date_of_birth: formData.dateOfBirth,
+    age: formData.age,
+    place_of_stay: formData.placeOfStay,
+    previous_experience: formData.previousExperience,
   };
 
-  const handlePasswordChange = (event) => {
-    const { name, value } = event.target;
-    const confirmPassword = basicInfo.confirmPassword;
+  try {
+    // Send the data to the backend using fetch or your preferred HTTP library
+    const response = await fetch('http://127.0.0.1:8000/api/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': document.cookie.match(/csrftoken=([^;]*)/)[1],
+      },
+      body: JSON.stringify(userData),
+    });
 
-    // Check if passwords match
-    setPasswordMatch(confirmPassword === value);
-
-    setBasicInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
-  };
-
-  const handleAdditionalInfoChange = (event) => {
-    const { name, value, type, checked } = event.target;
-    setAdditionalInfo((prevInfo) => ({
-      ...prevInfo,
-      [name]: type === 'checkbox' ? checked : value,
-    }));
-  };
-
+    if (response.ok) {
+      console.log('User created successfully');
+      // Reset the form after successful submission
+      setFormData({
+        firstName: '',
+        lastName: '',
+        email: '',
+        telePhone: '',
+        password: '',
+        confirmPassword: '',
+        profilePicture: null,
+        dateOfBirth: '',
+        age: '',
+        placeOfStay: '',
+        previousExperience: '',
+        acceptTerms: false,
+      });
+    } else {
+      console.error('Error creating user:', response.statusText);
+      // Handle errors or display error messages as needed
+    }
+  } catch (error) {
+    console.error('Error creating user:', error.message);
+    // Handle errors or display error messages as needed
+  }
+};
 
   return (
     <div className='flex flex-col items-center justify-center mt-5 h-full w-full'>
@@ -113,8 +131,8 @@ const SignUpForm = () => {
             fullWidth
             required
             name="firstName"
-            value={basicInfo.firstName}
-            onChange={handleBasicInfoChange}
+            value={formData.firstName}
+            onChange={handleChange}
           />
           <TextField
             label="Last Name"
@@ -123,8 +141,8 @@ const SignUpForm = () => {
             fullWidth
             required
             name="lastName"
-            value={basicInfo.lastName}
-            onChange={handleBasicInfoChange}
+            value={formData.lastName}
+            onChange={handleChange}
           />
           <TextField
             label="Email"
@@ -133,9 +151,9 @@ const SignUpForm = () => {
             fullWidth
             type="email"
             required
-            value={basicInfo.email}
+            value={formData.email}
             name="email"
-            onChange={handleBasicInfoChange}
+            onChange={handleChange}
           />
           <TextField
             label="Telephone"
@@ -144,8 +162,8 @@ const SignUpForm = () => {
             fullWidth
             type="tel"
             name="telePhone"
-            value={basicInfo.telePhone}
-            onChange={handleBasicInfoChange}
+            value={formData.telePhone}
+            onChange={handleChange}
           />
             <TextField
               label="Password"
@@ -155,8 +173,8 @@ const SignUpForm = () => {
               type="password"
               required
               name="password"
-              value={basicInfo.password}
-              onChange={handlePasswordChange}
+              value={formData.password}
+              onChange={handleChange}
             />
             <TextField
               label="Confirm Password"
@@ -166,22 +184,84 @@ const SignUpForm = () => {
               type="password"
               required
               name="confirmPassword"
-              value={basicInfo.confirmPassword}
-              onChange={handlePasswordChange}
+              value={formData.confirmPassword}
+              onChange={handleChange}
             />
           {/* Image input field */}
           <TextField
-              label="Profile Picture"
-              type="file"
+  label="Profile Picture"
+  type="file"
+  variant="outlined"
+  margin="normal"
+  fullWidth
+  InputLabelProps={{
+    shrink: true,
+  }}
+  value={formData.profilePicture}
+  onChange={handleChange}
+/>
+            <TextField
+              label="Date of Birth"
               variant="outlined"
               margin="normal"
               fullWidth
-              InputLabelProps={{
-                shrink: true,
-              }}
-              onChange={handleBasicInfoChange}
-              value={basicInfo.profilePicture}
+              type="date"
+              required
+              name="dateOfBirth"
+              value={formData.dateOfBirth}
+              onChange={handleChange}
             />
+            <TextField
+              label="Age"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              rows={3}
+              name="age"
+              value={formData.age}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Place of Stay"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              required
+              name="placeOfStay"
+              value={formData.placeOfStay}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Previous Experience"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              multiline
+              rows={3}
+              name="previousExperience"
+              value={formData.previousExperience}
+              onChange={handleChange}
+            />
+            
+<div>
+  <p>
+  "I acknowledge and agree to adhere to the company's code of conduct. This includes treating all colleagues, clients, and stakeholders with respect and professionalism. I will conduct myself ethically and follow all applicable laws and regulations."
+  </p>
+</div>
+
+<div className="flex items-center mt-3">
+              <input
+                type="checkbox"
+                id="acceptTerms"
+                name="acceptTerms"
+                checked={formData.acceptTerms}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="acceptTerms" className="ml-2">
+                I accept the <a href="/terms" className="text-blue-500">Terms and Conditions</a>
+              </label>
+            </div>
 
           {/* Link to login page on the same line */}
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -196,94 +276,7 @@ const SignUpForm = () => {
           </Button>
         </form>
       </Box>
-      <Box
-        sx={{
-          width: { xs: '90%', sm: '80%', md: '70%', lg: '30%' }, // Adjust width for different screen sizes
-          border: '2px solid #ccc',
-          padding: '20px',
-          borderRadius: '8px',
-          textAlign: 'center',
-          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-        }}
-        className='mt-10'
-      >
-        <form onSubmit={handleSubmit}>
-          {/* Other form fields */}
-          <TextField
-              label="Date of Birth"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              type="date"
-              required
-              name="dateOfBirth"
-              value={additionalInfo.dateOfBirth}
-              onChange={handleAdditionalInfoChange}
-            />
-            <TextField
-              label="Age"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              rows={3}
-              name="age"
-              value={additionalInfo.age}
-              onChange={handleAdditionalInfoChange}
-            />
-            <TextField
-              label="Place of Stay"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              required
-              name="placeOfStay"
-              value={additionalInfo.placeOfStay}
-              onChange={handleAdditionalInfoChange}
-            />
-
-<TextField
-              label="Previous Experience"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              multiline
-              rows={3}
-              name="previousExperience"
-              value={additionalInfo.previousExperience}
-              onChange={handleAdditionalInfoChange}
-            />
-
-
-
-<div>
-  <p>
-  "I acknowledge and agree to adhere to the company's code of conduct. This includes treating all colleagues, clients, and stakeholders with respect and professionalism. I will conduct myself ethically and follow all applicable laws and regulations."
-  </p>
-</div>
-
-<div className="flex items-center mt-3">
-              <input
-                type="checkbox"
-                id="acceptTerms"
-                name="acceptTerms"
-                checked={additionalInfo.acceptTerms}
-                onChange={handleAdditionalInfoChange}
-                required
-              />
-              <label htmlFor="acceptTerms" className="ml-2">
-                I accept the <a href="/terms" className="text-blue-500">Terms and Conditions</a>
-              </label>
-            </div>
-
-
-
-          <Button type="submit" variant="contained" color="primary" fullWidth>
-            Confirm
-          </Button>
-        </form>
-      </Box>
       </div>
-
     </div>
   );
 };
