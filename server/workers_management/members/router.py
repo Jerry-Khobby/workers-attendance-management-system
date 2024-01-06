93b5f29cbd9534acfe019fa4983cc4c1dfe4580f
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-01-06 06:50:28 -1200

    done with the api for loggin , now going to test using postman

--- server/workers_management/members/router.py
+++ server/workers_management/members/router.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from django.urls import path, include
 
 
-from .viewset import UserListCreateView,UserRetrieveCardView
+from .viewset import UserListCreateView,UserRetrieveCardView,UserLoginDetail
 
 app_name='members'
 
@@ -10,6 +10,7 @@ router=routers.DefaultRouter()
 
 router.register(r'users',UserListCreateView,basename='user-list-create')
 router.register(r'usercard', UserRetrieveCardView, basename='user-retrieve-card')
+router.register(r'userlogin',UserLoginDetail,basename='user-login')
 
 
 
