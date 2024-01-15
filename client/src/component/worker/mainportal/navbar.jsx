6ae257419641ae46a526671f6b516dfdc4bf29dd
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-01-15 05:22:53 -1200

    almost done with the first hamburger system

--- client/src/component/worker/mainportal/navbar.jsx
+++ client/src/component/worker/mainportal/navbar.jsx
@@ -1,28 +1,38 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes,faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-    // changing the icon from hamburger to cross
-const [crossIcon,setCrossIcon]=useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-const handleCrossIcon=()=>{
-setCrossIcon(true);
-}
+  const handleToggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <div className="flex flex-row items-center justify-around border-b border-gray-300 p-4">
-      <div>
-        <FontAwesomeIcon icon={faBars} size="x" className="text-black bg-gray-300 rounded-full p-2" />
-      </div>
+      <button onClick={handleToggleMenu} className="focus:outline-none">
+        <FontAwesomeIcon icon={showMenu ? faTimes : faBars} size="lg" className="text-black" />
+      </button>
       <div>
         <h2 className='font-mono'>
           JERRY COMPANY LIMITED
         </h2>
       </div>
       <div>
-        <FontAwesomeIcon icon={faUser} size="x" className="text-gray-500 rounded-full border border-gray-500 p-1" />
+      <FontAwesomeIcon icon={faUser} size="lg" className="text-gray-500 rounded-full border border-gray-500 p-1" />
       </div>
+      {showMenu && (
+        <div className="fixed top-0 left-0 h-full w-64 bg-gray-50 p-4">
+          <ul>
+            <li>Check In - Today</li>
+            <li>Check-out</li>
+            <li>Attendance record sheet</li>
+          </ul>
+        </div>
+      )}
+
+
     </div>
   );
 };
