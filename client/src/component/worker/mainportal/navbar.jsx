b5405dfd1bd2c788b0d8675da499f6f67ebcefda
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-01-15 06:11:11 -1200

    still working on the pop menu and it responsiveness

--- client/src/component/worker/mainportal/navbar.jsx
+++ client/src/component/worker/mainportal/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes,faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -10,9 +10,17 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex flex-row items-center justify-around border-b border-gray-300 p-4">
-      <button onClick={handleToggleMenu} className="focus:outline-none">
-        <FontAwesomeIcon icon={showMenu ? faTimes : faBars} size="lg" className="text-black" />
+    <div className="flex flex-row items-center justify-around border-b border-gray-300 p-4 relative">
+      <button
+        onClick={handleToggleMenu}
+        className="focus:outline-none relative z-10" // Ensure the button stays on top
+      >
+        <FontAwesomeIcon
+          icon={showMenu ? faTimes : faBars}
+          size="x"
+          className="text-black bg-gray-100 rounded-full border p-2"
+          style={{ zIndex: showMenu ? 1 : 0 }} // Set higher z-index when the menu is open
+        />
       </button>
       <div>
         <h2 className='font-mono'>
@@ -20,19 +28,29 @@ const Navbar = () => {
         </h2>
       </div>
       <div>
-      <FontAwesomeIcon icon={faUser} size="lg" className="text-gray-500 rounded-full border border-gray-500 p-1" />
+        <FontAwesomeIcon icon={faUser} size="x" className="text-gray-500 rounded-full border border-gray-500 p-1" />
       </div>
       {showMenu && (
-        <div className="fixed top-0 left-0 h-full w-64 bg-gray-50 p-4">
-          <ul>
-            <li>Check In - Today</li>
-            <li>Check-out</li>
-            <li>Attendance record sheet</li>
+        <div className="fixed top-0 left-0 h-full w-64 bg-gray-50 p-4 sm:h-full">
+          <ul className='flex flex-col items-center sm:items-start justify-around  sm:justify-center  h-full'>
+            <li>
+            <button className='text-black p-2 w-56 sm:w-56 text-center hover:bg-white hover:border hover:border-gray-300 rounded-md'>
+              Check In - Today
+              </button>
+              </li>
+            <li>
+            <button className='text-black p-2 w-56 text-center hover:bg-white hover:border hover:border-gray-300 rounded-md'>
+              Check-out
+                </button>
+                </li>
+            <li>
+            <button className='text-black p-2 w-56 text-center hover:bg-white hover:border hover:border-gray-300 rounded-md'>
+                Attendance record
+              </button>
+            </li>
           </ul>
         </div>
       )}
-
-
     </div>
   );
 };
