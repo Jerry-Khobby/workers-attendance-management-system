e5f9b6cad16684c92b9fd17583318c6a1a514f99
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-01-15 04:51:40 -1200

    I have started with the menu on the right side

--- client/src/component/worker/mainportal/navbar.jsx
+++ client/src/component/worker/mainportal/navbar.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-
-
-
+import React,{useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-    return (  
-        <div className="flex flex-row items-center justify-around h-11 w-full  ">
-            <div>
-             This is the hamburger   
-         </div> 
-            <div>
-                The name of the site
-            </div>
-            <div>
-                This is the account icon
-            </div>
-        </div>
-    );
+    // changing the icon from hamburger to cross
+const [crossIcon,setCrossIcon]=useState(false);
+
+const handleCrossIcon=()=>{
+setCrossIcon(true);
 }
- 
+
+  return (
+    <div className="flex flex-row items-center justify-around border-b border-gray-300 p-4">
+      <div>
+        <FontAwesomeIcon icon={faBars} size="x" className="text-black bg-gray-300 rounded-full p-2" />
+      </div>
+      <div>
+        <h2 className='font-mono'>
+          JERRY COMPANY LIMITED
+        </h2>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faUser} size="x" className="text-gray-500 rounded-full border border-gray-500 p-1" />
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
